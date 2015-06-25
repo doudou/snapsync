b78a5fd9aff98df971b981d76a5e0fd0dc98bc0c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-25 09:44:32 -0300

    use the logging gem for logging

--- lib/snapsync.rb
+++ lib/snapsync.rb
@@ -20,10 +20,32 @@ module Logging
 end
 
 class Module
-    def install_logging(forward: true)
-        extend Installer
+    def install_root_logging(forward: true, &block)
+        extend Logging::Installer
+        if block_given?
+            yield(logger)
+        else
+            logger.add_appenders Logging.appenders.stdout
+        end
         if forward
-            extend Forwarder
+            install_logging_forwarder
+        end
+    end
+
+    def install_logging_forwarder
+        ::Logging::LEVELS.each do |name, _|
+            define_method name do |*args, &block|
+                logger.send(name, *args, &block)
+            end
+        end
+    end
+
+    def install_logging(forward: true, &block)
+        extend Logging::Installer
+        if forward
+            singleton_class.class_eval do
+                install_logging_forwarder
+            end
         end
     end
 end
@@ -32,15 +54,15 @@ class Class
     def install_logging(forward: true, on_instance: false)
         super(forward: forward)
         if on_instance
-            include Installer
+            include Logging::Installer
             if forward
-                include Forwarder
+                install_logging_forwarder
             end
         end
     end
 end
 
 module Snapsync
-    install_logger(forward: true)
+    install_root_logging(forward: true)
 end
 
