7ba26dd8e7376afcb153132c009fd6798c7703d4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-25 13:33:02 -0300

    sync: improve progress report
    It now includes the estimated remaining time, and avoids the classical
    issues of having leftovers from previous message when using \r

--- lib/snapsync/local_sync.rb
+++ lib/snapsync/local_sync.rb
@@ -53,6 +53,8 @@ module Snapsync
 
                         puts "Estimating transfer to be #{human_readable_size(estimated_size)}"
 
+                        longest_message_length = 0
+                        receive_status, send_status = nil
                         IO.popen(['sudo', 'btrfs', 'send', *parent_opt, src.subvolume_dir.to_s]) do |send_io|
                             IO.popen(['sudo', 'btrfs', 'receive', target_snapshot_dir.to_s], 'w') do |receive_io|
                                 receive_io.sync = true
@@ -64,7 +66,16 @@ module Snapsync
 
                                         counter += buffer.size
                                         rate = counter / (Time.now - start)
-                                        STDOUT.print "\r#{human_readable_size(counter)} (#{human_readable_size(rate)}/s)"
+                                        remaining =
+                                            if estimated_size > counter
+                                                human_readable_time((estimated_size - counter) / rate)
+                                            else
+                                                '?'
+                                            end
+
+                                        msg = "#{human_readable_size(counter)} (#{human_readable_size(rate)}/s), #{remaining} remaining"
+                                        longest_message_length = [longest_message_length, msg.length].max
+                                        print "\r%#{longest_message_length}s" % [msg]
                                     end
                                 end
                                 rate = counter / (Time.now - start)
@@ -97,6 +108,13 @@ module Snapsync
                     last_common_snapshot = src
                 end
             end
+        end
+
+        def human_readable_time(time)
+            hrs = time / 3600
+            min = (time / 60) % 60
+            sec = time % 60
+            "%02i:%02i:%02i" % [hrs, min, sec]
         end
 
         def human_readable_size(size, digits: 1)
