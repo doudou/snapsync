e0535485de9a6386c9bca77a8b3658d98d6a68ca
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-03 10:57:45 -0300

    install: enable and start the systemd service
    This is allows to fail, though

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ if test -d /opt/snapsync; then
     sudo cp -r . /opt/snapsync
 fi
 
+if test -d /lib/systemd/system; then
+    snapsync_gem=`bundler show snapsync`
+    cp $snapsync_gem/snapsync.service /lib/systemd/system
+    ( systemctl enable snapsync.service
+      systemctl start snapsync.service )
+fi
+
 rm -rf $target
 
-        
