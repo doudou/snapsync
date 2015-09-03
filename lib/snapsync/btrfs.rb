b35990f2425690f32e539d654394aa710edfb4e7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-03 09:31:55 -0300

    allow calling Btrfs.popen without block
    In which case it returns the command's output

--- lib/snapsync/btrfs.rb
+++ lib/snapsync/btrfs.rb
@@ -23,11 +23,14 @@ module Snapsync
             block_error, block_result = nil
             IO.popen(['btrfs', *args, err: err_w, **options], mode) do |io|
                 err_w.close
-                begin
-                    block_result = yield(io)
-                rescue Error
-                    raise
-                rescue Exception => block_error
+                if block_given?
+                    begin
+                        block_result = yield(io)
+                    rescue Error
+                        raise
+                    rescue Exception => block_error
+                    end
+                else io.read
                 end
             end
 
